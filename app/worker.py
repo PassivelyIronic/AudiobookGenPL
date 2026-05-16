a67d89ca35b6e29f935479fbf969f5c1cd6199de
PassivelyIronic <jakubarczewski@gmail.com>
2026-05-16 16:45:51 +0200

    fix: naprawa budowania w CI

--- app/worker.py
+++ app/worker.py
@@ -45,8 +45,8 @@ _settings = get_settings()
 
 celery_app = Celery(
     "epub_narrate",
-    broker=settings.celery_broker_url,
-    backend=settings.celery_result_backend,
+    broker=_settings.celery_broker_url,
+    backend=_settings.celery_result_backend,
 )
 
 celery_app.conf.update(
